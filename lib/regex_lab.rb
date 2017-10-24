0a2ad41b81ae466e94e61bfea54056cf4d96066a
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-24 07:39:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,9 +18,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  text.match(/^[A-Z].+[\.!?]$/) ? true : false
 
 end
 
 def valid_phone_number?(phone)
+   phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
 
 end
